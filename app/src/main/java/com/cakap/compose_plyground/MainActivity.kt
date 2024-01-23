967d3a91630abaa91425819520904d775308833c
Januprasad K <jnuprasad@gmail.com>
2024-01-23 21:59:53 +0530

    add corner test

--- app/src/main/java/com/cakap/compose_plyground/MainActivity.kt
+++ app/src/main/java/com/cakap/compose_plyground/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -25,7 +27,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalConfiguration
@@ -50,8 +60,37 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    App() //pincode search
 //                    PhoneLayout() // flow row example
-                    Column {
-                        SimpleColumnDemo()
+//                    Column {
+//                        SimpleColumnDemo()
+//                    }
+                    Column(
+                        Modifier
+                            .fillMaxSize().padding(10.dp),
+                    ) {
+                        Spacer(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .drawWithContent {
+                                    val cornerRadius = CornerRadius(25f, 25f)
+                                    val path = Path().apply {
+                                        addRoundRect(
+                                            RoundRect(
+                                                rect = Rect(
+                                                    offset = Offset(0f, 0f),
+                                                    size = Size(200f, 200f),
+                                                ),
+                                                bottomRight = cornerRadius, // just an example
+                                                topRight = cornerRadius, // just an example
+                                            ),
+                                        )
+                                    }
+                                    drawPath(
+                                        path,
+                                        color = Color.Red,
+                                        style = Stroke(width = 1.dp.toPx()),
+                                    )
+                                },
+                        )
                     }
                 }
             }
@@ -173,13 +212,16 @@ fun FlipCard(pair: Pair<String, Int>, frontColor: Color, backColor: Color) {
             }
             .clickable {
                 rotated = !rotated
-            }.background(animateColor),
+            }
+            .background(animateColor),
     ) {
         val configuration = LocalConfiguration.current
         val widthInDp = configuration.screenWidthDp.dp
         val boxWidth = widthInDp.div(4)
         Column(
-            Modifier.size(boxWidth).border(1.dp, Color.LightGray),
+            Modifier
+                .size(boxWidth)
+                .border(1.dp, Color.LightGray),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
         ) {
@@ -199,4 +241,3 @@ fun FlipCard(pair: Pair<String, Int>, frontColor: Color, backColor: Color) {
         }
     }
 }
-
